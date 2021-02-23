91cdd1b2229f06dc8f0079fd22d5f37a77405655
Vaughan Schmidt <vaughan.schmidt@gmail.com>
2021-02-22 18:11:22 -0800

    Finish the farm assignment

--- farm.go
+++ farm.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 type Animal struct {
 	food string
@@ -20,9 +25,65 @@ func (a Animal) Speak() {
 	fmt.Println(a.noise)
 }
 
+func getActions() []string {
+	actions := make([]string, 0)
+	actions = append(actions, "eat", "move", "speak")
+	return actions
+}
+
+func getAnimals() []string {
+	animals := make([]string, 0)
+	animals = append(animals, "cow", "snamke", "bird")
+	return animals
+}
+
+func stringInSlice(s string, sl []string) bool {
+	for _, e := range sl {
+		if e == s {
+			return true
+		}
+	}
+	return false
+}
+
 func getUserCommand() (string, string) {
-	fmt.Print(">")
-	return "cow", "speak"
+	stdin := bufio.NewReader(os.Stdin)
+	var action, animal string
+
+	for {
+		fmt.Print("> ")
+		cmd, err := stdin.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error parsing your command, try again.")
+			continue
+		}
+		fields := strings.Fields(cmd)
+		animal = fields[0]
+		action = fields[1]
+		fmt.Println(animal, action)
+
+		if len(fields) != 2 {
+			fmt.Println("Error - did not find 2 tokens expected, try again.")
+			printInstructions()
+			continue
+		}
+
+		allowedAnimals := getAnimals()
+		if !stringInSlice(animal, allowedAnimals) {
+			fmt.Printf("Error - Animal %v not legal, must be one of %v \n", animal, allowedAnimals)
+			continue
+		}
+
+		allowedActions := getActions()
+		if !stringInSlice(action, allowedActions) {
+			_, _ = fmt.Printf("Error - Action %v not legal, must be one of %v  \n", action, allowedActions)
+			continue
+		}
+
+		break
+	}
+
+	return animal, action
 }
 
 func printInstructions() {
